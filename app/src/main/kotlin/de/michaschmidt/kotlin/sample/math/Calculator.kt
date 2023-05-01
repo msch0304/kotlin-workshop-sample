00caa31e3b0c0bdf9a05284f2028f47b4d786734
Michael Schmidt <m.schmidt@tarent.de>
2023-05-01 21:44:00 +0200

    Initial commit

---
+++ app/src/main/kotlin/de/michaschmidt/kotlin/sample/math/Calculator.kt
@@ -0,0 +1,11 @@
+package de.michaschmidt.kotlin.sample.math
+
+
+class Calculator {
+
+    fun <E> product(a:E, b:e): E = TODO()
+
+    fun <E> exponantiante (a:E, b: e) : E = TODO();
+
+    fun <E> average(list: List<E>) : E = TODO();
+}
---
+++ app/src/test/kotlin/de/michaschmidt/kotlin/sample/math/CalculatorTest.kt
@@ -0,0 +1,18 @@
+package de.michaschmidt.kotlin.sample.math
+
+import org.junit.jupiter.api.Assertions.*
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    fun product() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun exponantiante() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun average() {
+    }
+}

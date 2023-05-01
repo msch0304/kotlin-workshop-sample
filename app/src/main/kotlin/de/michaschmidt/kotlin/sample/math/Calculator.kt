0c813ddea749f787cca042c668b0bfa28b365efd
Michael Schmidt <m.schmidt@tarent.de>
2023-05-01 22:04:41 +0200

    fixed typo

--- app/src/main/kotlin/de/michaschmidt/kotlin/sample/math/Calculator.kt
+++ app/src/main/kotlin/de/michaschmidt/kotlin/sample/math/Calculator.kt
@@ -3,9 +3,9 @@ package de.michaschmidt.kotlin.sample.math
 
 class Calculator {
 
-    fun <E> product(a:E, b:e): E = TODO()
+    fun <E> product(a:E, b: E): E = TODO()
 
-    fun <E> exponantiante (a:E, b: e) : E = TODO();
+    fun <E> exponantiante (a:E, b: E) : E = TODO();
 
     fun <E> average(list: List<E>) : E = TODO();
 }

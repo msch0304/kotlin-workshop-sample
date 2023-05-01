e1c2d4ba381edb60ac8bcdd9d3350428fce5e7cb
Michael Schmidt <m.schmidt@tarent.de>
2023-05-01 22:03:52 +0200

    added absolute function

--- app/src/main/kotlin/de/michaschmidt/kotlin/sample/math/Calculator.kt
+++ app/src/main/kotlin/de/michaschmidt/kotlin/sample/math/Calculator.kt
@@ -5,7 +5,9 @@ class Calculator {
 
     fun <E> product(a:E, b: E): E = TODO()
 
-    fun <E> exponantiante (a:E, b: E) : E = TODO();
+    fun <E> exponantiante (a:E, b: E) : E = TODO()
 
-    fun <E> average(list: List<E>) : E = TODO();
+    fun <E> average(list: List<E>) : E = TODO()
+
+    fun <E> absolute(a: E) : E = TODO()
 }
